a22ccf5977535cf3237d5354a572172893888bf6
Aleksandar Ristic <aristic@software-nation.com>
2019-05-20 14:58:48 +0200

    Create Form user details component

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-
+import FormUserDetails from "./FormUserDetails";
 class UserForm extends Component {
   state = {
     step: 1,
@@ -31,13 +31,28 @@ class UserForm extends Component {
   handleChange = input => e => {
     this.setState({
       [input]: e.target.value
-    })
-  }
-  render() 
+    });
+  };
+  render() {
     const { step } = this.state;
     const { firstName, lastName, email, occupation, city, bio } = this.state;
-    const values = {};
-    return <div />;
+    const values = { firstName, lastName, email, occupation, city, bio };
+    switch (step) {
+      case 1:
+        return (
+          <FormUserDetails
+            nextStep={this.nextStep}
+            handleChange={this.handleChange}
+            values={values}
+          />
+        );
+      case 2:
+        return <h1>FormPersonalDetails</h1>;
+      case 3:
+        return <h1>Confirm</h1>;
+      case 4:
+        return <h1>Success</h1>;
+    }
   }
 }
 
