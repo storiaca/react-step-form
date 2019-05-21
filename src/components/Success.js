9e9b6e25ae0aa1880cda2be582e1df92bbb84960
Aleksandar Ristic <storiaca@gmail.com>
2019-05-21 15:35:00 +0200

    Create Success component

---
+++ src/components/Success.js
@@ -0,0 +1,16 @@
+import React from "react";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import AppBar from "material-ui/AppBar";
+const Success = () => {
+  return (
+    <MuiThemeProvider>
+      <React.Fragment>
+        <AppBar title="Success" />
+        <h1>Thank You For Your Submission</h1>
+        <p>You will get an email with further instructions</p>
+      </React.Fragment>
+    </MuiThemeProvider>
+  );
+};
+
+export default Success;
